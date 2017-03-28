8b6f71fb46213696b2ecd8014abb4ae883ae01ad
percytran96 <percytran96@gmail.com>
2017-03-28 13:47:09 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get "login" => "pages#login"
   get "gym" => "pages#gym"
   get "members" => "pages#members"
+  get "https://khang--rails--training.herokuapp.com" => "pages#members"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
